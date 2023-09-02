6829b1be90f6da795338f0f1079f7fa81294d5cd
Subhendu.io <Subhendu.luv@gmail.com>
2023-09-03 05:00:54 +0530

    Type error fixed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,7 +7,7 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options = {}
 
-let client
+let client: any;
 let clientPromise: Promise<MongoClient>
 
 if (!process.env.MONGODB_URI) {
@@ -17,11 +17,11 @@ if (!process.env.MONGODB_URI) {
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = (global as any)._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
